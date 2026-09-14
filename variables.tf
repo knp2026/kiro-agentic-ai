7a165e3346ddaa8be61aeff70995cdab0f5a687e
knp2026 <k.nayak.pradeep@accenture.com>
2026-09-14 16:51:14 +0200

    Codestral generated code

--- variables.tf
+++ variables.tf
@@ -1,16 +1,43 @@
 
-# Define the variables used in the main.tf file
-
-variable "instance_count" {
-  description = "Number of EC2 instances to create"
-  type        = number
-  default     = 1
-}
-
-variable "instance_type" {
-  description = "Type of EC2 instance to create"
+# Define the variables for the project
+variable "project_name" {
+  description = "The name of the project"
   type        = string
-  default     = "t2.micro"
+  default     = "my-project"
 }
 
-# Add more variables as needed for your project
+variable "environment" {
+  description = "The environment (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "region" {
+  description = "The AWS region for resources"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "availability_zones" {
+  description = "The AWS availability zones for resources"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "The CIDR blocks for the public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "The CIDR blocks for the private subnets"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
