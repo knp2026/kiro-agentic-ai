181acd20a349d9a6e60a28428d841b594816068b
knp2026 <k.nayak.pradeep@accenture.com>
2026-09-14 16:51:02 +0200

    Codestral generated code

--- repository/account_repository.py
+++ repository/account_repository.py
@@ -3,15 +3,15 @@ from sqlalchemy.orm import Session
 from models.account import Account
 from schemas.account import AccountCreate
 
-def get_account(db: Session, account_id: int):
-    return db.query(Account).filter(Account.AccountID == account_id).first()
-
-def get_accounts_by_customer_id(db: Session, customer_id: int):
-    return db.query(Account).filter(Account.CustomerID == customer_id).all()
-
 def create_account(db: Session, account: AccountCreate):
     db_account = Account(**account.dict())
     db.add(db_account)
     db.commit()
     db.refresh(db_account)
     return db_account
+
+def get_account_by_id(db: Session, account_id: int):
+    return db.query(Account).filter(Account.AccountID == account_id).first()
+
+def get_accounts_by_customer_id(db: Session, customer_id: int):
+    return db.query(Account).filter(Account.CustomerID == customer_id).all()
