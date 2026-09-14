54f74b3a7eca68d8d5eac1dfd9af5a1e3bf76f14
knp2026 <k.nayak.pradeep@accenture.com>
2026-09-14 16:41:35 +0200

    Codestarl generated code

---
+++ main.tf
@@ -0,0 +1,16 @@
+
+# Define the provider (AWS)
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Define the resources (EC2 instances, RDS database, etc.)
